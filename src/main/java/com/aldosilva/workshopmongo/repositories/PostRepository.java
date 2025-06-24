2e5097dd6b9834f4f5e1f6cc2bff3dfb7bd667f8
Aldo Silva <aldo.silvajr@outlook.com>
2025-06-23 21:40:33 -0300

    Consulta simple com @Query

--- src/main/java/com/aldosilva/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/aldosilva/workshopmongo/repositories/PostRepository.java
@@ -4,6 +4,7 @@ import com.aldosilva.workshopmongo.domain.Post;
 import com.aldosilva.workshopmongo.domain.User;
 import com.aldosilva.workshopmongo.dtos.AuthorDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByAuthorId(String authorId);
 
     List<Post> findByTitleContainingIgnoreCase (String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 
 }
